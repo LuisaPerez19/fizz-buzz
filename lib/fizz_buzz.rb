2f2f4a24b5f6208b3d94af2a611909e94caed742
Luisa Perez <luisa.perezw3@gmail.com>
2023-10-25 16:07:13 +0100

    adding Fizbuzz test and solution

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,21 @@
-class Fizzbuzz do
-
+class Fizzbuzz
+  def self.outputs
+    (1..100).map do |n|
+      if n % 3 == 0 && n % 5 == 0
+        'FizzBuzz'
+      elsif n % 3 == 0
+        'Fizz'
+      elsif n % 5 == 0
+        'Buzz'
+      else
+        n.to_s
+      end
+    end
+  end
 end
+
+# result = Fizzbuzz.outputs
+
+# result.each do |item|
+#   puts item
+# end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,4 +1,24 @@
 require "fizz_buzz"
 RSpec.describe Fizzbuzz do
+  subject(:outputs) { described_class.outputs }
+  it "starts at 1" do
+    expect(outputs.first).to eq('1')
+  end
+
+  it "has 100 outputs" do
+    expect(outputs.size).to eq(100)
+  end
+
+  it "outputs Fizz for multiples of 3" do
+    expect(outputs[2]).to eq('Fizz')
+    expect(outputs[5]).to eq('Fizz')
+  end
+  it "outputs Buzz for multiples of 5" do
+    expect(outputs[4]).to eq('Buzz')
+    expect(outputs[9]).to eq('Buzz')
+  end
+  it "outputs FizzBuzz for multiples of 3 and 5" do
+    expect(outputs[14]).to eq('FizzBuzz')
+  end
 
 end
